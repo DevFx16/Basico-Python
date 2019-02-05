8714eb3eee22ac1b82dce7ed2edfee7fef6328cc
Fernando Araujo <fernandoaraujo081@gmail.com>
2019-02-04 20:17:05 -0500

    Operaciones

---
+++ Operaciones.py
@@ -0,0 +1,16 @@
+x = 'Hola Mundo'
+y = 3
+z = 4
+
+print(x)
+print(y + z)
+print(y - z)
+print(y * z)
+print(y / z)
+print(y ** z)
+print(y == z)
+print(y != z)
+print(y > z)
+print(y < z)
+print(y <= z)
+print(y >= z)
