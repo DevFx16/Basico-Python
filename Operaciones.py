7dd816487ac97915a51bca64a2c1d4d59cbac9ee
Fernando Araujo <fernandoaraujo081@gmail.com>
2019-02-11 21:18:11 -0500

    Variables

--- Operaciones.py
+++ Operaciones.py
@@ -1,7 +1,10 @@
+#Strings
 x = 'Hola Mundo'
+#Numeros
 y = 3
 z = 4
 
+print('\nOperaciones')
 print(x)
 print(y + z)
 print(y - z)
@@ -14,3 +17,26 @@ print(y > z)
 print(y < z)
 print(y <= z)
 print(y >= z)
+print('\n')
+
+#Lista
+print('Lista')
+print([2,'Esto es una lista', True])
+print('\n')
+
+#Diccionario
+print('Diccionario')
+print({
+    'Hola':'Hola Mundo',
+    'Numero': 32,
+    'Logico': True
+})
+print('\n')
+
+#Tuples
+print('Tuples')
+print((2,3,4,5))
+print('\n')
+
+#none
+print(None)
