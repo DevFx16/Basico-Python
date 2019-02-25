799a7f516e0dd6c04ba0d278c0138ac6ec922832
DevelopGadget <fernandoaraujo081@gmail.com>
2019-02-25 10:52:25 -0500

    Bouncy

--- Bouncy.py
+++ Bouncy.py
@@ -1,33 +1,18 @@
-def Creciente(Num):
-    r = False
-    a = 0
-    while r == False and a < len(Num) - 1:
-        if int(Num[a]) <= int(Num[a + 1]):
-            if a + 1 == len(Num) - 1 :
-                r = True
-        else:
-            r = False
-            break
-        a = a + 1
-    return r
+NumBouncy = 0
+NoBouncy = 99
+x = 99
 
 
-def DeCreciente(Num):
-    r = False
-    a = 0
-    while r == False and a < len(Num) - 1:
-        if int(Num[a]) >= int(Num[a + 1]):
-            if a + 1 == len(Num) - 1 :
-                r = True
-        else:
-            r = False
-            break
-        a = a + 1
-    return r
+def NumeroBouncy(num):
+    CadNum = str(num)
+    CadNumOrdenado = ''.join(sorted(list(CadNum)))
+    return not(CadNum == CadNumOrdenado or CadNum == CadNumOrdenado[::-1])
 
 
-Num = input('Numero: ')
-if Creciente(Num) == False and DeCreciente(Num) == False:
-    print('Es Bouncy')
-else:
-    print('No Es Bouncy')
+while float(NumBouncy)/(NoBouncy + NumBouncy) < 0.99:
+    x += 1
+    if NumeroBouncy(x):
+        NumBouncy += 1
+    else:
+        NoBouncy += 1
+print(x)
